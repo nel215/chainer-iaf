34be4ada0e06995c5ac07b5e0ee5da9f9bea755c
nel215 <otomo.yuhei@gmail.com>
2018-02-17 20:04:13 +0900

    Add LowerTriangleLinear link

--- iaf/autoregressive.py
+++ iaf/autoregressive.py
@@ -1,6 +1,27 @@
+import numpy as np
 from chainer import Chain
 from chainer import links as L
 from chainer import functions as F
+from chainer.functions.connection import linear
+from chainer.links.connection.linear import functools, operator
+
+
+class LTLinear(L.Linear):
+    """
+    Lower Triangle Linear
+    """
+    def _initialize_params(self, in_size):
+        super(LTLinear, self)._initialize_params(in_size)
+        self.triu_indices = np.triu_indices(in_size, m=self.out_size)
+        self.W.data[self.triu_indices] = 0
+
+    def __call__(self, x):
+        if self.W.data is None:
+            in_size = functools.reduce(operator.mul, x.shape[1:], 1)
+            self._initialize_params(in_size)
+
+        self.W.data[self.triu_indices] = 0
+        return linear.linear(x, self.W, self.b)
 
 
 class AutoregressiveNN(Chain):
---
+++ iaf/test_lt_linear.py
@@ -0,0 +1,14 @@
+import numpy as np
+from numpy.testing import assert_almost_equal
+from iaf.autoregressive import LTLinear
+
+
+def test_call():
+    batch_size = 4
+    x_size = 16
+    z_size = 8
+    x = np.random.randn(batch_size, x_size).astype('f')
+    lt_linear = LTLinear(None, z_size)
+    lt_linear(x)
+    triu_indices = np.triu_indices(x_size, m=z_size)
+    assert_almost_equal(lt_linear.W.data[triu_indices], 0)
