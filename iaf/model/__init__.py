c88b63bc19e812e73ce08bb28a222ffd66c8d0d3
nel215 <otomo.yuhei@gmail.com>
2018-02-18 16:20:31 +0900

    Add a method to create a sample model

--- iaf/model/__init__.py
+++ iaf/model/__init__.py
@@ -3,3 +3,17 @@ from iaf.model.decoder import FCDecoder  # noqa
 from iaf.model.iaf import InverseAutoregressiveFlow, IAFBlock  # noqa
 from iaf.model.autoregressive import AutoregressiveLinear  # noqa
 from iaf.model.vae import VAE  # noqa
+
+
+def create_sample_model(n_x):
+    n_z = 100
+    n_h = 200
+    m_ann = AutoregressiveLinear(n_z)
+    s_ann = AutoregressiveLinear(n_z)
+    iaflow = InverseAutoregressiveFlow(*[
+        IAFBlock(m_ann, s_ann),
+    ])
+    decoder = FCDecoder(n_h, n_x)
+    model = VAE(n_z, n_h, iaflow, decoder)
+
+    return model
